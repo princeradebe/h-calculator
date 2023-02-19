e385c9630d3c18ac8be74c0258e635943cd54c03
Prince Radebe <princeradebe@gmail.com>
2023-02-19 20:40:31 +0200

    Add color to text inside the input field

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ function App() {
       <h1 className="text-2xl font-bold text-hubBlue mb-8">Huble Calculator</h1>
       {/*Top of calulator */}
       <div className="flex flex-col w-2/4 h-56 mb-8 bg-gray-200 rounded-lg border-2 border-hubBlue justify-center items-center">
-        <input type="number" className="w-22 h-12 mb-4 bg-gray-200 border-2 border-hubBlue text-right text-2xl" />
+        <input type="number" className="w-22 h-12 mb-4 bg-gray-200 border-2 border-hubBlue text-right text-2xl text-hubBlue" />
         <div className="flex gap-2">
           <button className="w-12 h-12 bg-hubBlue hover:bg-hubDarkBlue border-2 border-hubBlue text-white">+</button>
           <button className="w-12 h-12 bg-hubBlue hover:bg-hubDarkBlue border-2 border-hubBlue text-white">-</button>
           <button className="w-12 h-12 bg-hubBlue hover:bg-hubDarkBlue border-2 border-hubBlue text-white">*</button>
-          <button className="w-12 h-12 bg-hubBlue hover:bg-hubDarkBlue border-2 border-hubBlue text-red-900">/</button>
+          <button className="w-12 h-12 bg-hubBlue hover:bg-hubDarkBlue border-2 border-hubBlue">/</button>
         </div>
       </div>
       {/*Bottom of calulator */}
